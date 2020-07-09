9a054bd575dd9607df4459c9a7eb80074d184415
Luana Silveira e Silva Schlei <luana.schlei@gmail.com>
2020-07-09 16:06:47 +0200

    Introduction to Sets

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # HackerRank_Python
+
+## List of Python Practices Solved
+
+### Introduction to Sets
+
+[Introduction to Sets](https://www.hackerrank.com/challenges/py-introduction-to-sets/problem)
+[Resolution](py-introduction-to-sets/py-introduction-to-sets.py)
+
---
+++ py-introduction-to-sets/py-introduction-to-sets.py
@@ -0,0 +1,22 @@
+#
+# Url: https://www.hackerrank.com/challenges/py-introduction-to-sets/problem
+#
+# Title: Introduction to Sets
+#
+
+
+
+def average(array):
+
+    alturas_distintas_set = set(array)
+    alturas_distintas_len = len(alturas_distintas_set)
+    soma_alturas_distintas = sum(alturas_distintas_set)
+    media_alturas_distintas = soma_alturas_distintas / alturas_distintas_len
+
+    return media_alturas_distintas
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
