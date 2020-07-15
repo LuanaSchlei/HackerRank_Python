ec25f7d1b23de58f020e4535553b934ff2e3bd51
Luana Silveira e Silva Schlei <luana.schlei@gmail.com>
2020-07-15 13:10:06 +0200

    List comprehensions
    - https://www.hackerrank.com/challenges/list-comprehensions/problem

---
+++ list-comprehensions/list-comprehensions-teste.py
@@ -0,0 +1,17 @@
+
+
+
+
+
+if __name__ == '__main__':
+#    x = int(input())
+#    y = int(input())
+#    z = int(input())
+#    n = int(input())
+
+    x = 1
+    y = 1
+    z = 1
+    n = 2
+
+    print([[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range( z + 1) if (( i + j + k)  != n)])
