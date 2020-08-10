38c6d5d21d3bbdb85f1986065508bdc95cb91b3f
Luana Silveira e Silva Schlei <luana.schlei@gmail.com>
2020-08-10 11:25:11 +0200

    Collections.ordereddict()
    -  https://www.hackerrank.com/challenges/py-collections-ordereddict/problem

--- py-collections-ordereddict/py-collections-ordereddict-teste.py
+++ py-collections-ordereddict/py-collections-ordereddict-teste.py
@@ -3,7 +3,51 @@
 #
 # Url: https://www.hackerrank.com/challenges/py-collections-ordereddict/problem
 #
-# Title: py-collections-ordereddict
+# Title: Collections.ordereddict()
 #
 # Arquivo de Teste
 #
+
+from collections import OrderedDict
+
+input_numero_itens = '9'
+input_itens = [
+    'BANANA FRIES 12',
+    'POTATO CHIPS 30',
+    'APPLE JUICE 10',
+    'CANDY 5',
+    'APPLE JUICE 10',
+    'CANDY 5',
+    'CANDY 5',
+    'CANDY 5',
+    'POTATO CHIPS 30']
+
+# numero_itens = int(input())
+numero_itens = int(input_numero_itens)
+
+compra = OrderedDict()
+
+for i in range(numero_itens):
+    # item = input()
+    item = input_itens[i]
+    print(item)
+    item_lista = item.split()
+    print(item_lista)
+    valor_lista = item_lista[-1:]
+    valor_item = int(item_lista[-1:][0])
+    print(valor_item)
+    leng_preco = len(item_lista[-1:][0])
+    print(leng_preco)
+    leng_preco_aux = leng_preco * -1
+    print(leng_preco_aux)
+    nome_item = item[:leng_preco_aux].strip()
+    print(nome_item)
+    if nome_item in compra:
+        compra[nome_item] = compra[nome_item] + valor_item
+    else:
+        compra[nome_item] = valor_item
+
+print(compra)
+
+for i in compra:
+    print(i, compra[i])
--- py-collections-ordereddict/py-collections-ordereddict.py
+++ py-collections-ordereddict/py-collections-ordereddict.py
@@ -3,5 +3,28 @@
 #
 # Url: https://www.hackerrank.com/challenges/py-collections-ordereddict/problem
 #
-# Title: py-collections-ordereddict
+# Title: Collections.ordereddict()
 #
+
+from collections import OrderedDict
+
+numero_itens = numero_itens = int(input())
+
+compra = OrderedDict()
+
+for i in range(numero_itens):
+    item = input()
+    item_lista = item.split()
+    valor_lista = item_lista[-1:]
+    valor_item = int(item_lista[-1:][0])
+    leng_preco = len(item_lista[-1:][0])
+    leng_preco_aux = leng_preco * -1
+    nome_item = item[:leng_preco_aux].strip()
+    if nome_item in compra:
+        compra[nome_item] = compra[nome_item] + valor_item
+    else:
+        compra[nome_item] = valor_item
+
+for i in compra:
+    print(i, compra[i])
+
