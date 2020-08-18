736f5fa8aa880bcf2b075843bfd52e3fb338aea6
Luana Silveira e Silva Schlei <luana.schlei@gmail.com>
2020-08-18 14:10:42 +0200

    itertools.combinations_with_replacement()
    - https://www.hackerrank.com/challenges/itertools-combinations-with-replacement/problem

---
+++ itertools-combinations-with-replacement/itertools-combinations-with-replacement-teste.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+#
+# Url: https://www.hackerrank.com/challenges/itertools-combinations-with-replacement/problem
+#
+# Title: itertools.combinations_with_replacement()
+#
+# Arquivo de Teste
+#
+
+from itertools import combinations_with_replacement
+
+input_s_k = 'HACK 4'
+split_input = input_s_k.split()
+
+input_s = split_input[0]
+s_upper = input_s.upper()
+list_s = list(s_upper)
+# print(list_s)
+s_sorted = sorted(list_s)
+# print(s_sorted)
+# s_letra_por_linha = '\n'.join(map(lambda x: str(x), s_sorted))
+# print(s_letra_por_linha)
+
+input_k = split_input[1]
+k_int = int(input_k)
+
+# s_letra_por_linha = ''.join(map(str, s_sorted))
+# print(s_letra_por_linha)
+s_lista = list(combinations_with_replacement(s_sorted, k_int))
+# print(s_lista)
+s_combi = list(map(lambda x: ''.join(x), s_lista))
+# print(s_combi)
+s_result = '\n'.join(s_combi)
+print(s_result)
